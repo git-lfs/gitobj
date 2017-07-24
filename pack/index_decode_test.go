c3b1e4d668c385bb4d16646d3dd284f8522a267d
Taylor Blau <me@ttaylorr.com>
2017-07-24 13:28:56 -0600

    git/odb/pack: promote IndexVersion to interface

--- pack/index_decode_test.go
+++ pack/index_decode_test.go
@@ -24,7 +24,6 @@ func TestDecodeIndexV2(t *testing.T) {
 	idx, err := DecodeIndex(bytes.NewReader(buf))
 
 	assert.NoError(t, err)
-	assert.Equal(t, V2, idx.version)
 	assert.EqualValues(t, 3, idx.Count())
 }
 
@@ -44,7 +43,6 @@ func TestDecodeIndexV1(t *testing.T) {
 	idx, err := DecodeIndex(bytes.NewReader(make([]byte, indexFanoutWidth)))
 
 	assert.NoError(t, err)
-	assert.Equal(t, V1, idx.version)
 	assert.EqualValues(t, 0, idx.Count())
 }
 
