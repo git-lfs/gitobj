git-lfs/git-lfs

git/odb/pack: introduce ErrNotFound

git/odb/pack/index.go
@@ -2,6 +2,8 @@ package pack
 
 import (
 	"io"
+
+	"github.com/git-lfs/git-lfs/errors"
 )
 
 // Index stores information about the location of objects in a corresponding
@@ -26,6 +28,18 @@ type Index struct {
 // Count returns the number of objects in the packfile.
 func (i *Index) Count() int {
 	return int(i.fanout[255])
+}
+
+var (
+	// errNotFound is an error returned by Index.Entry() (see: below) when
+	// an object cannot be found in the index.
+	errNotFound = errors.New("git/odb/pack: object not found in index")
+)
+
+// IsNotFound returns whether a given error represents a missing object in the
+// index.
+func IsNotFound(err error) bool {
+	return err == errNotFound
 }
 
 // Entry returns an entry containing the offset of a given SHA1 "name".

git-lfs/gitobj

pack/index.go
@@ -1,6 +1,7 @@
 package pack
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -26,6 +27,18 @@ type Index struct {
 // Count returns the number of objects in the packfile.
 func (i *Index) Count() int {
 	return int(i.fanout[255])
+}
+
+var (
+	// errNotFound is an error returned by Index.Entry() (see: below) when
+	// an object cannot be found in the index.
+	errNotFound = fmt.Errorf("git/odb/pack: object not found in index")
+)
+
+// IsNotFound returns whether a given error represents a missing object in the
+// index.
+func IsNotFound(err error) bool {
+	return err == errNotFound
 }
 
 // Entry returns an entry containing the offset of a given SHA1 "name".