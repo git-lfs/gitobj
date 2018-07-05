git-lfs/git-lfs

git/odb: introduce package-specific errors

git/odb/errors.go
@@ -1,0 +1,20 @@
+package odb
+
+import "fmt"
+
+// UnexpectedObjectType is an error type that represents a scenario where an
+// object was requested of a given type "Wanted", and received as a different
+// _other_ type, "Wanted".
+type UnexpectedObjectType struct {
+	// Got was the object type requested.
+	Got ObjectType
+	// Wanted was the object type received.
+	Wanted ObjectType
+}
+
+var _ error = (*UnexpectedObjectType)(nil)
+
+// Error implements the error.Error() function.
+func (e *UnexpectedObjectType) Error() string {
+	return fmt.Sprintf("git/odb: unexpected object type, got: %q, wanted: %q", e.Got, e.Wanted)
+}

git/odb/errors_test.go
@@ -1,0 +1,15 @@
+package odb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnexpectedObjectTypeErrFormatting(t *testing.T) {
+	err := &UnexpectedObjectType{
+		Got: TreeObjectType, Wanted: BlobObjectType,
+	}
+
+	assert.Equal(t, "git/odb: unexpected object type, got: \"tree\", wanted: \"blob\"", err.Error())
+}

git-lfs/gitobj

errors.go
@@ -1,0 +1,20 @@
+package gitobj
+
+import "fmt"
+
+// UnexpectedObjectType is an error type that represents a scenario where an
+// object was requested of a given type "Wanted", and received as a different
+// _other_ type, "Wanted".
+type UnexpectedObjectType struct {
+	// Got was the object type requested.
+	Got ObjectType
+	// Wanted was the object type received.
+	Wanted ObjectType
+}
+
+var _ error = (*UnexpectedObjectType)(nil)
+
+// Error implements the error.Error() function.
+func (e *UnexpectedObjectType) Error() string {
+	return fmt.Sprintf("git/odb: unexpected object type, got: %q, wanted: %q", e.Got, e.Wanted)
+}

errors_test.go
@@ -1,0 +1,15 @@
+package gitobj
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnexpectedObjectTypeErrFormatting(t *testing.T) {
+	err := &UnexpectedObjectType{
+		Got: TreeObjectType, Wanted: BlobObjectType,
+	}
+
+	assert.Equal(t, "git/odb: unexpected object type, got: \"tree\", wanted: \"blob\"", err.Error())
+}