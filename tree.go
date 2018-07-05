git-lfs/git-lfs

git/odb: introduce *odb.Tree type

git/odb/tree.go
@@ -1,0 +1,123 @@
+package odb
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// Tree encapsulates a Git tree object.
+type Tree struct {
+	// Entries is the list of entries held by this tree.
+	Entries []*TreeEntry
+}
+
+var _ Object = (*Tree)(nil)
+
+// Type implements Object.ObjectType by returning the correct object type for
+// Trees, TreeObjectType.
+func (t *Tree) Type() ObjectType { return TreeObjectType }
+
+// Decode implements Object.Decode and decodes the uncompressed tree being
+// read. It returns the number of uncompressed bytes being consumed off of the
+// stream, which should be strictly equal to the size given.
+//
+// If any error was encountered along the way, that will be returned, along with
+// the number of bytes read up to that point.
+func (t *Tree) Decode(from io.Reader, size int64) (n int, err error) {
+	buf := bufio.NewReader(from)
+
+	var entries []*TreeEntry
+	for {
+		modes, err := buf.ReadString(' ')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return n, err
+		}
+		n += len(modes)
+		modes = strings.TrimSuffix(modes, " ")
+		modes = fmt.Sprintf("%06s", modes)
+
+		mode, _ := strconv.ParseInt(modes, 8, 32)
+
+		fname, err := buf.ReadString('\x00')
+		if err != nil {
+			return n, err
+		}
+		n += len(fname)
+		fname = strings.TrimSuffix(fname, "\x00")
+
+		var sha [20]byte
+		if _, err = buf.Read(sha[:]); err != nil {
+			return n, err
+		}
+		n += 20
+
+		var typ ObjectType
+		switch mode & syscall.S_IFMT {
+		case syscall.S_IFREG:
+			typ = BlobObjectType
+		case syscall.S_IFDIR:
+			typ = TreeObjectType
+		case syscall.S_IFLNK:
+			typ = BlobObjectType
+		default:
+			panic(fmt.Sprintf("git/odb: unknown object type: %q %q", modes, fname))
+		}
+
+		entries = append(entries, &TreeEntry{
+			Name:     fname,
+			Type:     typ,
+			Oid:      sha[:],
+			Filemode: int32(mode),
+		})
+	}
+
+	t.Entries = entries
+
+	return n, nil
+}
+
+// Encode encodes the tree's contents to the given io.Writer, "w". If there was
+// any error copying the tree's contents, that error will be returned.
+//
+// Otherwise, the number of bytes written will be returned.
+func (t *Tree) Encode(to io.Writer) (n int, err error) {
+	const entryTmpl = "%s %s\x00%s"
+
+	for _, entry := range t.Entries {
+		fmode := strconv.FormatInt(int64(entry.Filemode), 8)
+
+		ne, err := fmt.Fprintf(to, entryTmpl,
+			fmode,
+			entry.Name,
+			entry.Oid)
+
+		if err != nil {
+			return n, err
+		}
+
+		n = n + ne
+	}
+	return
+}
+
+// TreeEntry encapsulates information about a single tree entry in a tree
+// listing.
+type TreeEntry struct {
+	// Name is the entry name relative to the tree in which this entry is
+	// contained.
+	Name string
+	// Type is the type of entry (either blob: BlobObjectType, or a
+	// sub-tree: TreeObjectType).
+	Type ObjectType
+	// Oid is the object ID for this tree entry.
+	Oid []byte
+	// Filemode is the filemode of this tree entry on disk.
+	Filemode int32
+}

git/odb/tree_test.go
@@ -1,0 +1,103 @@
+package odb
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTreeReturnsCorrectObjectType(t *testing.T) {
+	assert.Equal(t, TreeObjectType, new(Tree).Type())
+}
+
+func TestTreeEncoding(t *testing.T) {
+	tree := &Tree{
+		Entries: []*TreeEntry{
+			{
+				Name:     "a.dat",
+				Type:     BlobObjectType,
+				Oid:      []byte("aaaaaaaaaaaaaaaaaaaa"),
+				Filemode: 0100644,
+			},
+			{
+				Name:     "subdir",
+				Type:     TreeObjectType,
+				Oid:      []byte("bbbbbbbbbbbbbbbbbbbb"),
+				Filemode: 040000,
+			},
+		},
+	}
+
+	buf := new(bytes.Buffer)
+
+	n, err := tree.Encode(buf)
+	assert.Nil(t, err)
+	assert.NotEqual(t, 0, n)
+
+	assertTreeEntry(t, buf, "a.dat", BlobObjectType, []byte("aaaaaaaaaaaaaaaaaaaa"), 0100644)
+	assertTreeEntry(t, buf, "subdir", TreeObjectType, []byte("bbbbbbbbbbbbbbbbbbbb"), 040000)
+
+	assert.Equal(t, 0, buf.Len())
+}
+
+func TestTreeDecoding(t *testing.T) {
+	from := new(bytes.Buffer)
+	fmt.Fprintf(from, "%s %s\x00%s",
+		strconv.FormatInt(int64(0100644), 8),
+		"a.dat", []byte("aaaaaaaaaaaaaaaaaaaa"))
+	fmt.Fprintf(from, "%s %s\x00%s",
+		strconv.FormatInt(int64(040000), 8),
+		"subdir", []byte("bbbbbbbbbbbbbbbbbbbb"))
+	fmt.Fprintf(from, "%s %s\x00%s",
+		strconv.FormatInt(int64(0120000), 8),
+		"symlink", []byte("cccccccccccccccccccc"))
+
+	flen := from.Len()
+
+	tree := new(Tree)
+	n, err := tree.Decode(from, int64(flen))
+
+	assert.Nil(t, err)
+	assert.Equal(t, flen, n)
+
+	require.Equal(t, 3, len(tree.Entries))
+	assert.Equal(t, &TreeEntry{
+		Name:     "a.dat",
+		Type:     BlobObjectType,
+		Oid:      []byte("aaaaaaaaaaaaaaaaaaaa"),
+		Filemode: 0100644,
+	}, tree.Entries[0])
+	assert.Equal(t, &TreeEntry{
+		Name:     "subdir",
+		Type:     TreeObjectType,
+		Oid:      []byte("bbbbbbbbbbbbbbbbbbbb"),
+		Filemode: 040000,
+	}, tree.Entries[1])
+	assert.Equal(t, &TreeEntry{
+		Name:     "symlink",
+		Type:     BlobObjectType,
+		Oid:      []byte("cccccccccccccccccccc"),
+		Filemode: 0120000,
+	}, tree.Entries[2])
+}
+
+func assertTreeEntry(t *testing.T, buf *bytes.Buffer,
+	name string, typ ObjectType, oid []byte, mode int32) {
+
+	fmode, err := buf.ReadBytes(' ')
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(strconv.FormatInt(int64(mode), 8)+" "), fmode)
+
+	fname, err := buf.ReadBytes('\x00')
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(name+"\x00"), fname)
+
+	var sha [20]byte
+	_, err = buf.Read(sha[:])
+	assert.Nil(t, err)
+	assert.Equal(t, oid, sha[:])
+}

git-lfs/gitobj

tree.go
@@ -1,0 +1,123 @@
+package gitobj
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// Tree encapsulates a Git tree object.
+type Tree struct {
+	// Entries is the list of entries held by this tree.
+	Entries []*TreeEntry
+}
+
+var _ Object = (*Tree)(nil)
+
+// Type implements Object.ObjectType by returning the correct object type for
+// Trees, TreeObjectType.
+func (t *Tree) Type() ObjectType { return TreeObjectType }
+
+// Decode implements Object.Decode and decodes the uncompressed tree being
+// read. It returns the number of uncompressed bytes being consumed off of the
+// stream, which should be strictly equal to the size given.
+//
+// If any error was encountered along the way, that will be returned, along with
+// the number of bytes read up to that point.
+func (t *Tree) Decode(from io.Reader, size int64) (n int, err error) {
+	buf := bufio.NewReader(from)
+
+	var entries []*TreeEntry
+	for {
+		modes, err := buf.ReadString(' ')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return n, err
+		}
+		n += len(modes)
+		modes = strings.TrimSuffix(modes, " ")
+		modes = fmt.Sprintf("%06s", modes)
+
+		mode, _ := strconv.ParseInt(modes, 8, 32)
+
+		fname, err := buf.ReadString('\x00')
+		if err != nil {
+			return n, err
+		}
+		n += len(fname)
+		fname = strings.TrimSuffix(fname, "\x00")
+
+		var sha [20]byte
+		if _, err = buf.Read(sha[:]); err != nil {
+			return n, err
+		}
+		n += 20
+
+		var typ ObjectType
+		switch mode & syscall.S_IFMT {
+		case syscall.S_IFREG:
+			typ = BlobObjectType
+		case syscall.S_IFDIR:
+			typ = TreeObjectType
+		case syscall.S_IFLNK:
+			typ = BlobObjectType
+		default:
+			panic(fmt.Sprintf("git/odb: unknown object type: %q %q", modes, fname))
+		}
+
+		entries = append(entries, &TreeEntry{
+			Name:     fname,
+			Type:     typ,
+			Oid:      sha[:],
+			Filemode: int32(mode),
+		})
+	}
+
+	t.Entries = entries
+
+	return n, nil
+}
+
+// Encode encodes the tree's contents to the given io.Writer, "w". If there was
+// any error copying the tree's contents, that error will be returned.
+//
+// Otherwise, the number of bytes written will be returned.
+func (t *Tree) Encode(to io.Writer) (n int, err error) {
+	const entryTmpl = "%s %s\x00%s"
+
+	for _, entry := range t.Entries {
+		fmode := strconv.FormatInt(int64(entry.Filemode), 8)
+
+		ne, err := fmt.Fprintf(to, entryTmpl,
+			fmode,
+			entry.Name,
+			entry.Oid)
+
+		if err != nil {
+			return n, err
+		}
+
+		n = n + ne
+	}
+	return
+}
+
+// TreeEntry encapsulates information about a single tree entry in a tree
+// listing.
+type TreeEntry struct {
+	// Name is the entry name relative to the tree in which this entry is
+	// contained.
+	Name string
+	// Type is the type of entry (either blob: BlobObjectType, or a
+	// sub-tree: TreeObjectType).
+	Type ObjectType
+	// Oid is the object ID for this tree entry.
+	Oid []byte
+	// Filemode is the filemode of this tree entry on disk.
+	Filemode int32
+}

tree_test.go
@@ -1,0 +1,103 @@
+package gitobj
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTreeReturnsCorrectObjectType(t *testing.T) {
+	assert.Equal(t, TreeObjectType, new(Tree).Type())
+}
+
+func TestTreeEncoding(t *testing.T) {
+	tree := &Tree{
+		Entries: []*TreeEntry{
+			{
+				Name:     "a.dat",
+				Type:     BlobObjectType,
+				Oid:      []byte("aaaaaaaaaaaaaaaaaaaa"),
+				Filemode: 0100644,
+			},
+			{
+				Name:     "subdir",
+				Type:     TreeObjectType,
+				Oid:      []byte("bbbbbbbbbbbbbbbbbbbb"),
+				Filemode: 040000,
+			},
+		},
+	}
+
+	buf := new(bytes.Buffer)
+
+	n, err := tree.Encode(buf)
+	assert.Nil(t, err)
+	assert.NotEqual(t, 0, n)
+
+	assertTreeEntry(t, buf, "a.dat", BlobObjectType, []byte("aaaaaaaaaaaaaaaaaaaa"), 0100644)
+	assertTreeEntry(t, buf, "subdir", TreeObjectType, []byte("bbbbbbbbbbbbbbbbbbbb"), 040000)
+
+	assert.Equal(t, 0, buf.Len())
+}
+
+func TestTreeDecoding(t *testing.T) {
+	from := new(bytes.Buffer)
+	fmt.Fprintf(from, "%s %s\x00%s",
+		strconv.FormatInt(int64(0100644), 8),
+		"a.dat", []byte("aaaaaaaaaaaaaaaaaaaa"))
+	fmt.Fprintf(from, "%s %s\x00%s",
+		strconv.FormatInt(int64(040000), 8),
+		"subdir", []byte("bbbbbbbbbbbbbbbbbbbb"))
+	fmt.Fprintf(from, "%s %s\x00%s",
+		strconv.FormatInt(int64(0120000), 8),
+		"symlink", []byte("cccccccccccccccccccc"))
+
+	flen := from.Len()
+
+	tree := new(Tree)
+	n, err := tree.Decode(from, int64(flen))
+
+	assert.Nil(t, err)
+	assert.Equal(t, flen, n)
+
+	require.Equal(t, 3, len(tree.Entries))
+	assert.Equal(t, &TreeEntry{
+		Name:     "a.dat",
+		Type:     BlobObjectType,
+		Oid:      []byte("aaaaaaaaaaaaaaaaaaaa"),
+		Filemode: 0100644,
+	}, tree.Entries[0])
+	assert.Equal(t, &TreeEntry{
+		Name:     "subdir",
+		Type:     TreeObjectType,
+		Oid:      []byte("bbbbbbbbbbbbbbbbbbbb"),
+		Filemode: 040000,
+	}, tree.Entries[1])
+	assert.Equal(t, &TreeEntry{
+		Name:     "symlink",
+		Type:     BlobObjectType,
+		Oid:      []byte("cccccccccccccccccccc"),
+		Filemode: 0120000,
+	}, tree.Entries[2])
+}
+
+func assertTreeEntry(t *testing.T, buf *bytes.Buffer,
+	name string, typ ObjectType, oid []byte, mode int32) {
+
+	fmode, err := buf.ReadBytes(' ')
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(strconv.FormatInt(int64(mode), 8)+" "), fmode)
+
+	fname, err := buf.ReadBytes('\x00')
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(name+"\x00"), fname)
+
+	var sha [20]byte
+	_, err = buf.Read(sha[:])
+	assert.Nil(t, err)
+	assert.Equal(t, oid, sha[:])
+}