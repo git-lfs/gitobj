37c4950812dceff72cbb11f62d0b24b0c9ae829e
brian m. carlson <bk2204@github.com>
2022-09-28 15:33:02 +0000

    commit: improve error messages
    Right now, the error message when trying to parse an invalid tree or
    parent header is hard to pin down because it's simply a generic hex
    string parsing error.  Make the error easier to discover in the future
    by adding some descriptive text to indicate to the user what the problem
    is.

--- commit.go
+++ commit.go
@@ -123,13 +123,13 @@ func (c *Commit) Decode(hash hash.Hash, from io.Reader, size int64) (n int, err 
 			case "tree":
 				id, err := hex.DecodeString(fields[1])
 				if err != nil {
-					return n, err
+					return n, fmt.Errorf("error parsing tree: %s", err)
 				}
 				c.TreeID = id
 			case "parent":
 				id, err := hex.DecodeString(fields[1])
 				if err != nil {
-					return n, err
+					return n, fmt.Errorf("error parsing parent: %s", err)
 				}
 				c.ParentIDs = append(c.ParentIDs, id)
 			case "author":
